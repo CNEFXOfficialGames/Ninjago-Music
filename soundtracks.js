74b0da48a5b12687b14319fdc84f06aca6ab6340
CNEFX <jellisvandenberg@gmail.com>
2026-06-20 15:51:16 +0200

    Update soundtracks.js

--- soundtracks.js
+++ soundtracks.js
@@ -1,1 +1,22 @@
-
+const songs = [
+  {
+    title: "Ninjago Theme",
+    url: "https://www.youtube.com/results?search_query=ninjago+theme"
+  },
+  {
+    title: "Kai Theme",
+    url: "https://www.youtube.com/results?search_query=kai+ninjago+theme"
+  },
+  {
+    title: "Jay Theme",
+    url: "https://www.youtube.com/results?search_query=jay+ninjago+theme"
+  },
+  {
+    title: "Lloyd Theme",
+    url: "https://www.youtube.com/results?search_query=lloyd+ninjago+theme"
+  },
+  {
+    title: "Ninjago Battle Music",
+    url: "https://www.youtube.com/results?search_query=ninjago+battle+music"
+  }
+];
